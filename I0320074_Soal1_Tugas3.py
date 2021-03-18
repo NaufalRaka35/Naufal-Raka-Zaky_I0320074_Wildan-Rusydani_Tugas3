55ef2f63f901197a9002bff2607275d94f39f385
Naufal Raka Zaky <nrakazaky35@gmail.com>
2021-03-18 22:06:06 +0700

    soal 1

---
+++ I0320074_Soal1_Tugas3.py
@@ -0,0 +1,20 @@
+# List 10 teman
+teman = ['Bagus','Attar','Adhi','Afnan','Nauval','Rizal','Memed','Uddin','Hilmy','Tito']
+
+# Menampilkan list indeks nomor 4,6, dan 7
+print("List indeks nomor 4,6, dan 7 adalah",teman[4],",",teman[6],", dan",teman[7])
+
+# Mengganti nama teman di list 3,5, dan 9
+teman[3] = 'Aji'
+teman[5] = 'Issa'
+teman[9] = 'Rio'
+
+# Menambahkan 2 nama teman
+teman.append('Sony')
+teman.append('Ano')
+
+# Menampilkan semua teman dengan pengulangan
+print(teman*2)
+
+# Menampilkan panjang list
+print(len(teman))
